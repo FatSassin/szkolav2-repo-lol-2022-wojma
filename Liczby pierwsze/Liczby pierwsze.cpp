2ff9f9cd88fb601f268a2ad568d689c5776a3ac8
FatSassin <hubert.talwik@gmail.com>
2022-03-07 14:52:24 +0100

    phony town

--- Liczby pierwsze/Liczby pierwsze.cpp
+++ Liczby pierwsze/Liczby pierwsze.cpp
@@ -2,5 +2,32 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+  
 }
+
+/* potrafi opisać teoretyczne działanie algorytmu
+- zna składnie języka C++ i wymienia jej mechanizmy
+- potrafi stworzyć schemat blokowy oraz pseudokod na podstawie opisywanego algorytmu
+- analizuje, modeluje i rozwiązuje sytuacje problemowe z różnych dziedzin
+3 (dst) Spełnia kryteria oceny dopuszczającej oraz:
+- opisuje własności algorytmu na podstawie ich analizy
+- stosuje podejście algorytmiczne do rozwiązania problemu
+- zna podstawowe mechanizmy działania języka C++ i umie je opisać
+- dobiera efektywny algorytm do rozwiązania sytuacji problemowej
+- ocenia zgodność algorytmu z specyfikacją problemu
+4 (db) Spełnia kryteria oceny dostatecznej oraz:
+- implementacja rozwiązań problemów algorytmicznych w wybranej notacji (C++, schemat
+blokowy, pseudokod)
+- potrafi zastosować podstawowe mechanizmy języka C++ w celu rozwiązania danego
+problemu
+- wykorzystuje platformę GitHub do zapisywania programów
+5 (bd) Spełnia kryteria oceny dobrej oraz:
+- bez problemu potrafi zaimplementować algorytm
+- bada efektywność komputerowych rozwiązań problemów
+- dobiera odpowiednie struktury danych do realizacji algorytmu
+- oblicza liczbę operacji wykonywanych przez algorytm
+6 (cel) Spełnia kryteria oceny bardzo dobrej oraz:
+- dobiera najlepsze rozwiązanie algorytmu i odpowiednie struktury danych do rozwiązania
+postawionego problemu
+- dokumentuje swoją pracę, wykorzystując platformę GitHub*/
+
