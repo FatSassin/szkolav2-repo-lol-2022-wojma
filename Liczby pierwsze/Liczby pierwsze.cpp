89237bb8112e514cc48a0e65d68a72af668ee6fc
FatSassin <hubert.talwik@gmail.com>
2022-03-14 14:31:02 +0100

    Twoja stara kod zrodlowy

--- Liczby pierwsze/Liczby pierwsze.cpp
+++ Liczby pierwsze/Liczby pierwsze.cpp
@@ -1,8 +1,31 @@
 ﻿#include <iostream>
-
+using namespace std;
 int main()
 {
-  
+    int n = 0;
+    cout << "ile liczb pierwszych mam wyswietlic?" << endl;
+    cin >> n;
+    int p = 2;
+    cout << "Liczby pierwsze:" << endl;
+    for (int c = 0; c < n; c)
+    {
+        bool pier = true;
+        for (int i = 2; i <= p / 2; i++)
+        {
+            if (p % i == 0)
+            {
+                pier = false;
+                break;
+            }
+        }
+        if (pier == true)
+        {
+            cout << p << endl;
+            c++;
+        }
+        p++;
+
+    }
 }
 
 /* potrafi opisać teoretyczne działanie algorytmu
