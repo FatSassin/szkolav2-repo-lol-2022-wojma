bca172de75d43f6d4b942754c3bb95d82e9f2aa3
Bartosz Borzyskowski (Uczeń) <bborzyskowski345@technikumkreatywne.pl>
2022-04-11 14:45:01 +0200

    hahahahaha

---
+++ szyfrcezara/szyfrcezara/szyfrcezara.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <chrono>
+using namespace std::chrono;
+using namespace std;
+void Fun(int kl, char tab[], string sl);
+void Fun(int kl, char tab[], string sl)
+{
+	int dl;
+	dl = strlen(tab);
+	if (!(kl >= -26 && kl <= 26))
+	{
+		cout << "zly klucz";
+
+	}
+	if (kl >= 0)
+	{
+		for (int i = 0; i < dl; i++)
+		{
+			if (tab[i] + kl <= 'z')
+			{
+				tab[i] += kl;
+			}
+			else
+			{
+				tab[i] = tab[i] + kl - 26;
+			}
+		}
+	}
+	else
+	{
+		for (int i = 0; i < dl; i++)
+		{
+			if (tab[i] + kl >= 'a')
+			{
+				tab[i] += kl;
+			}
+			else
+			{
+				tab[i] = tab[i] + kl + 26;
+			}
+		}
+	}
+	cout << "przed zaszyfrowaniem " << sl << endl;
+	cout << "po zaszyfrowaniu " << tab << endl;
+}
+int main()
+{
+	string sl;
+	int kl;
+	char tab[100];
+	cout << "wyraz do zaszyfrowania" << endl;
+	cin >> tab;
+	sl = tab;
+	cout << "podaj klucz z przedzialu od -26 do 26" << endl;
+	cin >> kl;
+	auto start = high_resolution_clock::now();
+	Fun(kl, tab, sl);
+	auto stop = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop - start);
+	cout << "czas wykonania: " << duration.count() << " ms";
+}
