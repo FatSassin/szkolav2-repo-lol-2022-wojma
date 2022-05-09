4c2df7bb45cfb8c3165370851f200ff9a7d35005
Bartosz Borzyskowski (Uczeń) <bborzyskowski345@technikumkreatywne.pl>
2022-05-09 14:51:39 +0200

    napis

---
+++ nww nwd/nww nwd/nww nwd.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a = 0;
+    int b = 0;
+    cout << "Podaj dwie liczby\n";
+    cin >> a >> b;
+    int iloczyn = a * b;
+    while (a != b)
+    {
+        if (a <= b)
+        {
+            b = b - a;
+        }
+        else
+        {
+            a = a - b;
+        }
+    }
+    float nww = iloczyn / a;
+    cout << "Najwieszkszy wspolny dzielnik = " << a << endl;
+    cout << "Najmniejsza wspojna wielokrotnosc = " << nww;
+}
